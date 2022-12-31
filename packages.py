de8f46411fef89b129d8c037a2d5d2aa9ed30929
Devon Hooper <devon.hooper91@gmail.com>
2022-12-30 22:48:50 -0500

    final commit 12/28

--- packages.py
+++ packages.py
@@ -23,15 +23,31 @@ for idx, value in enumerate(package_data_reader.get_first_delivery()):
 # this for loop compares the addresses on truck one to the list of addresses and adds the address index to the list
 # Space-time complexity is O(N^2)
 try:
-    first_variable_count = 0
+    first_count = 0
     for k in first_delivery:
         for j in distance.evaluate_address():
             if k[2] == j[2]:
                 first_truck_distances.append(j[0])
-                first_delivery[first_variable_count][1] = j[0]
-        first_variable_count += 1
+                first_delivery[first_count][1] = j[0]
+        first_count += 1
 except IndexError:
     pass
 # calls to the greedy algorithm that sorts the packages in a more efficient order
 distance.evaluate_shortest_distance(first_delivery, 1, 0)
 first_truck_total_distance = 0
+
+# this for loop takes the values in the first truck and runs them through the distance functions in the Distances.py file
+# Space-time complexity is O(N)
+first_truck_package_id = 0
+for index in range(len(distance.first_truck_index())):
+    try:
+        # calculate the total distance of the truck
+        first_truck_total_distance = distance.evaluate_distance(int(distance.first_truck_index()[index]), int(distance.first_truck_index()[index + 1]), first_truck_total_distance)
+        # calculate the distance of each package along the route
+        deliver_package = distance.get_time(distance.evaluate_current_distance(int(distance.first_truck_index()[index]), int(distance.first_truck_index()[index + 1])), first_truck_leave_time)
+        distance.first_truck_list()[first_truck_package_id][10] = (str(deliver_package))
+        package_data_reader.get_hash_map().update(int(distance.first_truck_list()[first_truck_package_id][0]), first_delivery)
+        first_truck_package_id += 1
+    except IndexError:
+        pass
+
